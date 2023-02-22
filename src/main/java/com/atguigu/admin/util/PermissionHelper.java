9637efee7d42e5c4501799ca2d7c9ca0c61d9b31
Emotioncai <582516879@qq.com>
2023-02-22 23:54:22 +0800

    2023/02/22

---
+++ src/main/java/com/atguigu/admin/util/PermissionHelper.java
@@ -0,0 +1,43 @@
+package com.atguigu.admin.util;
+import com.atguigu.admin.bean.Permission;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * 根据权限数据构建菜单数据
+ */
+public class PermissionHelper {
+    /**
+     * 使用递归方法建菜单
+     */
+    public static List<Permission> build(List<Permission> treeNodes) {
+        List<Permission> trees = new ArrayList<>();
+        for (Permission treeNode : treeNodes) {
+            if (treeNode.getParentId() == 0) {
+                treeNode.setLevel(1);
+                trees.add(findChildren(treeNode, treeNodes));
+            }
+        }
+        return trees;
+    }
+
+    /**
+     * 递归查找子节点
+     */
+    public static Permission findChildren(Permission treeNode, List<Permission> treeNodes) {
+        treeNode.setChildren(new ArrayList<>());
+
+        for (Permission it : treeNodes) {
+            if (Objects.equals(treeNode.getId(), it.getParentId())) {
+                it.setLevel(treeNode.getLevel() + 1);
+                if (treeNode.getChildren() == null) {
+                    treeNode.setChildren(new ArrayList<>());
+                }
+                treeNode.getChildren().add(findChildren(it, treeNodes));
+            }
+        }
+        return treeNode;
+    }
+}
